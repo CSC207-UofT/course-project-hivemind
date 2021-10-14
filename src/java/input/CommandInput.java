96cad3bac48988970465a155adccba787976621f
NathanW001 <nathan.wootton1@gmail.com>
2021-10-14 16:55:57 -0400

    finished basic input system, need to wait until controllers are done to finish actually making working commands

--- src/java/input/CommandInput.java
+++ src/java/input/CommandInput.java
@@ -1,9 +1,41 @@
 package input;
+import java.util.Scanner;
 
 public class CommandInput {
-    // TODO: implement this class
 
-    public CommandInput() {
+    public static void main (String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        String lastCommand;
+
+        while (true) {
+            System.out.print("> ");
+            lastCommand = myObj.nextLine();
+            parseInput(lastCommand);
+        }
 
     }
+
+    public static void parseInput(String input) {
+        String[] splitInput = parseInputHelper(input);
+        switch(splitInput[0]) {
+            case "exit":
+                System.exit(0);
+            case "food":
+                System.out.println("food lol");
+                break;
+            case "recipe":
+                System.out.println("recipe lol");
+                break;
+            default:
+                System.out.println("Error: command not recognized");
+        }
+    }
+
+    public static String[] parseInputHelper(String input) {
+        return input.split(" ");
+    }
+
+//    public static void handleFood(String ) {
+//
+//    }
 }
