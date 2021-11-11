46cd0654da6c0813d146b017e426277bcd051581
NathanW001 <nathan.wootton1@gmail.com>
2021-11-11 00:37:27 -0500

    added bottomnavbar to app, moved around program files

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.*;
+import input.CommandInput;
 
 public class MainActivity extends AppCompatActivity {
 
