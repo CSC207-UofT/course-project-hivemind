f3964ac43fe2484fc3158a30408af2c6aa1e5ac4
kevintan999 <kevintan999@gmail.com>
2021-12-04 15:49:32 -0500

    HVM-98-Added adapters to fragments

--- app/src/main/java/com/example/foodapp/SettingsFragment.java
+++ app/src/main/java/com/example/foodapp/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.foodapp;
 
+import adapters.Adapter;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
  */
 public class SettingsFragment extends Fragment {
 
+    Adapter adapter;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -24,6 +26,10 @@ public class SettingsFragment extends Fragment {
 
     public SettingsFragment() {
         // Required empty public constructor
+    }
+
+    public SettingsFragment(Adapter a){
+        adapter = a;
     }
 
     /**
