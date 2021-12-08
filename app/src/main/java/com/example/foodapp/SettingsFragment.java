9fd8f4a102a34bbb1910944f13d6a470551cdb32
kevintan999 <kevintan999@gmail.com>
2021-12-08 05:17:47 -0500

    HVM-119-documented and fixed intellij style errors for SettingsFragment

--- app/src/main/java/com/example/foodapp/SettingsFragment.java
+++ app/src/main/java/com/example/foodapp/SettingsFragment.java
@@ -1,10 +1,6 @@
 package com.example.foodapp;
-
-import adapters.Adapter;
 import android.os.Bundle;
 import android.widget.*;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,42 +28,43 @@ public class SettingsFragment extends Fragment {
     TextView fontExample;
     TextView recipeText;
 
-//    int barProgress = 50;
-
-    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-
+    /** Required Empty public constructor
+     */
     public SettingsFragment() {
         // Required empty public constructor
     }
-
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment SettingsFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        //        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
+
+    public static SettingsFragment newInstance() {
         return new SettingsFragment();
     }
 
+    /** Runs the super class's onCreate for settings fragment
+     *
+     * @param savedInstanceState Saved instance state as a bundle
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
     }
 
+
+    /** Displays a variety of settings pieces:
+     * -Settings Text
+     * -Text size seekbar
+     * -recipe text
+     * -Recipe number seekbar
+     *
+     * @param inflater inflater for settings fragment
+     * @param container container for settings fragment
+     * @param savedInstanceState saved instance state for settings fragment
+     * @return view for default
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -97,7 +94,6 @@ public class SettingsFragment extends Fragment {
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
@@ -123,8 +119,6 @@ public class SettingsFragment extends Fragment {
 
             }
         });
-
-
 
         seekbar.setProgress(fontSize);
         recipeBar.setProgress(recipeAmount);
